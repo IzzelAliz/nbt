6df35ec68ae4bb93a83c56e863ca222178391186
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-10-22 23:37:25 +0800

    Get default tags lazily

--- src/main/java/io/izzel/nbt/TagType.java
+++ src/main/java/io/izzel/nbt/TagType.java
@@ -6,21 +6,22 @@ import io.izzel.nbt.util.ImmutableLongs;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public enum TagType {
-    END(0, "TAG_End", EndTag.of()),
-    BYTE(1, "TAG_Byte", ByteTag.of((byte) 0)),
-    SHORT(2, "TAG_Short", ShortTag.of((short) 0)),
-    INT(3, "TAG_Int", IntTag.of(0)),
-    LONG(4, "TAG_Long", LongTag.of(0L)),
-    FLOAT(5, "TAG_Float", FloatTag.of(0F)),
-    DOUBLE(6, "TAG_Double", DoubleTag.of(0D)),
-    BYTE_ARRAY(7, "TAG_Byte_Array", ByteArrayTag.of(ImmutableBytes.empty())),
-    STRING(8, "TAG_String", StringTag.of("")),
-    LIST(9, "TAG_List", ListTag.empty()),
-    COMPOUND(10, "TAG_Compound", CompoundTag.empty()),
-    INT_ARRAY(11, "TAG_Int_Array", IntArrayTag.of(ImmutableInts.empty())),
-    LONG_ARRAY(12, "TAG_Long_Array", LongArrayTag.of(ImmutableLongs.empty()));
+    END(0, "TAG_End", EndTag::of),
+    BYTE(1, "TAG_Byte", () -> ByteTag.of((byte) 0)),
+    SHORT(2, "TAG_Short", () -> ShortTag.of((short) 0)),
+    INT(3, "TAG_Int", () -> IntTag.of(0)),
+    LONG(4, "TAG_Long", () -> LongTag.of(0L)),
+    FLOAT(5, "TAG_Float", () -> FloatTag.of(0F)),
+    DOUBLE(6, "TAG_Double", () -> DoubleTag.of(0D)),
+    BYTE_ARRAY(7, "TAG_Byte_Array", () -> ByteArrayTag.of(ImmutableBytes.empty())),
+    STRING(8, "TAG_String", () -> StringTag.of("")),
+    LIST(9, "TAG_List", ListTag::empty),
+    COMPOUND(10, "TAG_Compound", CompoundTag::empty),
+    INT_ARRAY(11, "TAG_Int_Array", () -> IntArrayTag.of(ImmutableInts.empty())),
+    LONG_ARRAY(12, "TAG_Long_Array", () -> LongArrayTag.of(ImmutableLongs.empty()));
 
     private static final Map<String, TagType> BY_NAME = new HashMap<>();
 
@@ -32,10 +33,12 @@ public enum TagType {
 
     private final int id;
     private final String tagName;
-    private final Tag defaultTag;
+    private final Supplier<Tag> defaultSupplier;
 
-    TagType(int id, String tagName, Tag defaultTag) {
-        this.defaultTag = defaultTag;
+    private Tag defaultTag;
+
+    TagType(int id, String tagName, Supplier<Tag> defaultSupplier) {
+        this.defaultSupplier = defaultSupplier;
         this.tagName = tagName;
         this.id = id;
     }
@@ -49,6 +52,9 @@ public enum TagType {
     }
 
     public Tag getDefault() {
+        if (defaultTag == null) {
+            defaultTag = defaultSupplier.get();
+        }
         return defaultTag;
     }
 
