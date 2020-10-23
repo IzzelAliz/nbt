54d484d1d4717d21d7c9f6a9233a08c24ad357e5
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-10-24 01:12:51 +0800

    Suppress exceptions thrown in nbt writer and rethrow them together

--- src/main/java/io/izzel/nbt/util/NbtWriter.java
+++ src/main/java/io/izzel/nbt/util/NbtWriter.java
@@ -13,26 +13,22 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
 public class NbtWriter extends TagValueVisitor implements Closeable {
 
     private final DataOutputStream data;
-    private final String name;
-    private final boolean tagType;
+    private final List<IOException> suppressed;
 
-    public NbtWriter(OutputStream stream) {
-        super(null);
-        this.name = "";
-        this.tagType = true;
-        this.data = stream instanceof DataOutputStream ? (DataOutputStream) stream : new DataOutputStream(stream);
+    public NbtWriter(OutputStream stream) throws IOException {
+        this(stream instanceof DataOutputStream ? (DataOutputStream) stream : new DataOutputStream(stream), "", new ArrayList<>(1));
     }
 
-    public NbtWriter(OutputStream stream, String name) {
-        super(null);
-        this.name = name;
-        this.tagType = true;
-        this.data = stream instanceof DataOutputStream ? (DataOutputStream) stream : new DataOutputStream(stream);
+    public NbtWriter(OutputStream stream, String name) throws IOException {
+        this(stream instanceof DataOutputStream ? (DataOutputStream) stream : new DataOutputStream(stream), name, new ArrayList<>(1));
     }
 
     public NbtWriter(OutputStream stream, boolean gzip) throws IOException {
@@ -45,248 +41,308 @@ public class NbtWriter extends TagValueVisitor implements Closeable {
 
     @Override
     public void close() throws IOException {
-        this.data.close();
+        this.closeParentQuietly();
+        this.throwException();
     }
 
-    @Override
-    public void visitEnd() {
+    private void closeParentQuietly() {
         try {
-            if (tagType) data.write(0);
+            this.data.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            this.suppressed.add(e);
         }
     }
 
-    @Override
-    public void visitByte(byte b) {
-        try {
-            if (tagType) data.write(1);
-            this.maybeWriteName();
-            data.writeByte(b);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitShort(short s) {
-        try {
-            if (tagType) data.write(2);
-            this.maybeWriteName();
-            data.writeShort(s);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitInt(int i) {
-        try {
-            if (tagType) data.write(3);
-            this.maybeWriteName();
-            data.writeInt(i);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitLong(long l) {
-        try {
-            if (tagType) data.write(4);
-            this.maybeWriteName();
-            data.writeLong(l);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitFloat(float f) {
-        try {
-            if (tagType) data.write(5);
-            this.maybeWriteName();
-            data.writeFloat(f);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitDouble(double d) {
-        try {
-            if (tagType) data.write(6);
-            this.maybeWriteName();
-            data.writeDouble(d);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitByteArray(ImmutableBytes bytes) {
-        try {
-            if (tagType) data.write(7);
-            this.maybeWriteName();
-            int len = bytes.size();
-            data.writeInt(len);
-            byte[] bufferArray = new byte[8192];
-            ByteBuffer buffer = ByteBuffer.wrap(bufferArray);
-            for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
-                 (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
-                buffer.put(ImmutableBytes.slice(bytes, offset, bufferStep).toByteArray());
-                data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
-                buffer.rewind();
+    public void throwException() throws IOException {
+        Iterator<IOException> iterator = this.suppressed.iterator();
+        if (iterator.hasNext()) {
+            IOException exception = iterator.next();
+            while (iterator.hasNext()) {
+                IOException next = iterator.next();
+                next.addSuppressed(exception);
+                exception = next;
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw exception;
         }
     }
 
-    @Override
-    public void visitString(String s) {
-        try {
-            if (tagType) data.write(8);
-            this.maybeWriteName();
-            byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
-            data.writeShort(bytes.length);
-            data.write(bytes, 0, bytes.length);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public TagListVisitor visitList() {
-        try {
-            if (tagType) data.write(9);
-            this.maybeWriteName();
-            return new ListWriter(this.data);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public TagCompoundVisitor visitCompound() {
-        try {
-            if (tagType) data.write(10);
-            this.maybeWriteName();
-            return new CompoundWriter(this.data);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitIntArray(ImmutableInts ints) {
-        try {
-            if (tagType) data.write(11);
-            this.maybeWriteName();
-            int len = ints.size();
-            data.writeInt(len);
-            byte[] bufferArray = new byte[8192];
-            IntBuffer buffer = ByteBuffer.wrap(bufferArray).asIntBuffer();
-            for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
-                 (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
-                buffer.put(ImmutableInts.slice(ints, offset, bufferStep).toIntArray());
-                data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
-                buffer.rewind();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void visitLongArray(ImmutableLongs longs) {
-        try {
-            if (tagType) data.write(12);
-            this.maybeWriteName();
-            int len = longs.size();
-            data.writeInt(len);
-            byte[] bufferArray = new byte[8192];
-            LongBuffer buffer = ByteBuffer.wrap(bufferArray).asLongBuffer();
-            for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
-                 (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
-                buffer.put(ImmutableLongs.slice(longs, offset, bufferStep).toLongArray());
-                data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
-                buffer.rewind();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void maybeWriteName() throws IOException {
-        if (this.name != null) {
-            byte[] bytes = this.name.getBytes(StandardCharsets.UTF_8);
-            data.writeShort(bytes.length);
-            data.write(bytes, 0, bytes.length);
-        }
-    }
-
-    private NbtWriter(DataOutputStream data, String name, boolean tagType) {
-        super(null);
+    private NbtWriter(DataOutputStream data, String name, List<IOException> suppressed) {
+        super(new ValueWriter(data, name, suppressed));
+        this.suppressed = suppressed;
         this.data = data;
-        this.name = name;
-        this.tagType = tagType;
+    }
+
+    private static final class ValueWriter extends TagValueVisitor {
+
+        private final List<IOException> suppressed;
+        private final DataOutputStream data;
+        private final String name;
+
+        private ValueWriter(DataOutputStream data, String name, List<IOException> suppressed) {
+            super(null);
+            this.name = name;
+            this.data = data;
+            this.suppressed = suppressed;
+        }
+
+        @Override
+        public void visitEnd() {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.END);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitByte(byte b) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.BYTE);
+                    this.data.writeByte(b);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitShort(short s) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.SHORT);
+                    this.data.writeShort(s);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitInt(int i) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.INT);
+                    this.data.writeInt(i);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitLong(long l) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.LONG);
+                    this.data.writeLong(l);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitFloat(float f) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.FLOAT);
+                    this.data.writeFloat(f);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitDouble(double d) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.DOUBLE);
+                    this.data.writeDouble(d);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitByteArray(ImmutableBytes bytes) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.BYTE_ARRAY);
+                    int len = bytes.size();
+                    data.writeInt(len);
+                    byte[] bufferArray = new byte[8192];
+                    ByteBuffer buffer = ByteBuffer.wrap(bufferArray);
+                    for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
+                         (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
+                        buffer.put(ImmutableBytes.slice(bytes, offset, bufferStep).toByteArray());
+                        data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
+                        buffer.rewind();
+                    }
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitString(String s) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.STRING);
+                    byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+                    data.writeShort(bytes.length);
+                    data.write(bytes, 0, bytes.length);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public TagListVisitor visitList() {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.LIST);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+            return new ListWriter(this.data, this.suppressed);
+        }
+
+        @Override
+        public TagCompoundVisitor visitCompound() {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.COMPOUND);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+            return new CompoundWriter(this.data, this.suppressed);
+        }
+
+        @Override
+        public void visitIntArray(ImmutableInts ints) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.INT_ARRAY);
+                    int len = ints.size();
+                    data.writeInt(len);
+                    byte[] bufferArray = new byte[8192];
+                    IntBuffer buffer = ByteBuffer.wrap(bufferArray).asIntBuffer();
+                    for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
+                         (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
+                        buffer.put(ImmutableInts.slice(ints, offset, bufferStep).toIntArray());
+                        data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
+                        buffer.rewind();
+                    }
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        @Override
+        public void visitLongArray(ImmutableLongs longs) {
+            if (this.suppressed.isEmpty()) {
+                try {
+                    this.writePrefix(TagType.LONG_ARRAY);
+                    int len = longs.size();
+                    data.writeInt(len);
+                    byte[] bufferArray = new byte[8192];
+                    LongBuffer buffer = ByteBuffer.wrap(bufferArray).asLongBuffer();
+                    for (int bufferLimit = buffer.limit(), offset = 0, bufferStep;
+                         (bufferStep = Math.min(bufferLimit, len - offset)) > 0; offset += bufferStep) {
+                        buffer.put(ImmutableLongs.slice(longs, offset, bufferStep).toLongArray());
+                        data.write(bufferArray, 0, 8192 * bufferStep / bufferLimit);
+                        buffer.rewind();
+                    }
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
+            }
+        }
+
+        private void writePrefix(TagType type) throws IOException {
+            if (this.name != null) {
+                data.write(type.getId());
+                byte[] bytes = this.name.getBytes(StandardCharsets.UTF_8);
+                data.writeShort(bytes.length);
+                data.write(bytes, 0, bytes.length);
+            }
+        }
     }
 
     private static class ListWriter extends TagListVisitor {
 
         private final DataOutputStream data;
+        private final List<IOException> suppressed;
 
-        public ListWriter(DataOutputStream data) {
+        public ListWriter(DataOutputStream data, List<IOException> suppressed) {
             super(null);
             this.data = data;
+            this.suppressed = suppressed;
         }
 
         @Override
         public void visitType(TagType tagType) {
-            try {
-                data.write(tagType.getId());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if (this.suppressed.isEmpty()) {
+                try {
+                    data.write(tagType.getId());
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
             }
         }
 
         @Override
         public void visitLength(int length) {
-            try {
-                data.writeInt(length);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if (this.suppressed.isEmpty()) {
+                try {
+                    data.writeInt(length);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
             }
         }
 
         @Override
         public TagValueVisitor visitValue() {
-            return new NbtWriter(this.data, null, false);
+            return new ValueWriter(this.data, null, this.suppressed);
         }
     }
 
     private static class CompoundWriter extends TagCompoundVisitor {
 
         private final DataOutputStream data;
+        private final List<IOException> suppressed;
 
-        public CompoundWriter(DataOutputStream data) {
+        public CompoundWriter(DataOutputStream data, List<IOException> suppressed) {
             super(null);
             this.data = data;
+            this.suppressed = suppressed;
         }
 
         @Override
         public TagValueVisitor visit(String key) {
-            return new NbtWriter(this.data, key, true);
+            return new ValueWriter(this.data, key, this.suppressed);
         }
 
         @Override
         public void visitEnd() {
             super.visitEnd();
-            try {
-                data.write(0);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if (this.suppressed.isEmpty()) {
+                try {
+                    data.write(0);
+                } catch (IOException e) {
+                    this.suppressed.add(e);
+                }
             }
         }
     }
